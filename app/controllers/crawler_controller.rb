4c7438a27a43e4fb178eb215d702e45996bc1890
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-08-07 13:06:15 -0400

    Fix parser, crawler ahora revisa al paginacion, se modifico el filtro de busqueda, y se formatearon datos como el valor, el año la fecha, y la cantidad de visualizaciones.

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -21,7 +21,7 @@ class CrawlerController < ApplicationController
 				model_field = form.field_with(:name => 'modelo')
 				model_field.value = search
 				ai_field = form.field_with(:name => 'ai')
-				ai_field.value = 2005
+				ai_field.value = 2012
 				af_field = form.field_with(:name => 'af')
 				af_field.value = 2016
 				dea_field = form.field_with(:name => 'dea')
@@ -40,34 +40,49 @@ class CrawlerController < ApplicationController
 						image: tr.css('td')[0].css('img')[0]['src'],
 						link: 'http:' + tr.css('td')[1].css('a')[0]['href'],
 						brand_model: tr.css('td')[1].css('a')[0].text.squish,
-						year: tr.css('td')[2].text.squish,
-						price: tr.css('td')[3].text.squish,
+						year: tr.css('td')[2].text.squish.to_i,
+						price: tr.css('td')[3].text.squish.gsub('$','').gsub('.','').gsub(' ','').to_i,
 						seller: tr.css('td')[4].text.squish,
-						published: published,
-						viewed: viewed
+						published: published.gsub('Publicado ',''),
+						viewed: viewed.to_i
 					}
 					result << element
 				rescue StandardError => e
-					error_obj << tr
+					# error_obj << tr
 					puts "Parse error #{e.message}"
-					# puts tr.css('td')[0].css('img')[0]['src']
-					# puts 'http:' + tr.css('td')[1].css('a')[0]['href']
-					# puts tr.css('td')[1].css('a')[0].text.squish
-					# puts tr.css('td')[1]
-					# puts tr.css('td')[2]
-					# puts tr.css('td')[3]
-					# puts tr.css('td')[4]
-					# puts tr.css('td')[2].text.squish
-					# puts tr.css('td')[3].text.squish
-					# puts tr.css('td')[4].text.squish
-					# puts tr.inspect
+				end
+			end
+			## Reviso las otras paginas
+			body.css('.nav').each do |link|
+				document = Nokogiri::HTML(open('http:'+link['href']))
+				document.css('.tbl_Principal').css('tr').each do |tr|
+					begin
+						url = 'http:' + tr.css('td')[1].css('a')[0]['href']
+						detail = Nokogiri::HTML(open(url))
+						published = detail.css('body > div:nth-child(6) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)')[0].text.squish
+						viewed = detail.css('.visitas-auto')[0].text.squish
+						element = {
+							image: tr.css('td')[0].css('img')[0]['src'],
+							link: 'http:' + tr.css('td')[1].css('a')[0]['href'],
+							brand_model: tr.css('td')[1].css('a')[0].text.squish,
+							year: tr.css('td')[2].text.squish.to_i,
+							price: tr.css('td')[3].text.squish.gsub('$','').gsub('.','').gsub(' ','').to_i,
+							seller: tr.css('td')[4].text.squish,
+							published: published.gsub('Publicado ',''),
+							viewed: viewed.to_i
+						}
+						result << element
+					rescue StandardError => e
+						# error_obj << tr
+						puts "Parse error #{e.message}"
+					end
 				end
 			end
 		end
 		if result.any?
 			render :json => { :status => true, :message => "Search Results.", :result => result }, :status => 200
 		else
-			render :json => { :status => true, :message => "There was a problem retrieving the information.", :error_obj => error_obj }, :status => 200
+			render :json => { :status => true, :message => "There was a problem retrieving the information." }, :status => 200
 		end
 	end
 end
