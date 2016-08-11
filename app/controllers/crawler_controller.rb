9d009a04fb7144cf5f14407f4547b4034f86aafc
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-08-11 01:43:16 -0400

    Test

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -16,7 +16,7 @@ class CrawlerController < ApplicationController
 		result = Array.new
 		error_obj = Array.new
 		a = Mechanize.new
-		a.set_proxy('127.0.0.1', '8050') # single tor proxy for now
+		# a.set_proxy('127.0.0.1', '8050')
 		a.get('http://www2.chileautos.cl/chileautos.asp') do |page|
 			search_result = page.form_with(:name => 'form_vehiculos') do |form|
 				model_field = form.field_with(:name => 'modelo')
@@ -60,6 +60,7 @@ class CrawlerController < ApplicationController
 					begin
 						url = 'http:' + tr.css('td')[1].css('a')[0]['href']
 						detail = Nokogiri::HTML(open(url, :proxy => "http://127.0.0.1:8050"))
+						# detail = Nokogiri::HTML(open(url, :proxy => "http://127.0.0.1:8050"))
 						published = detail.css('body > div:nth-child(6) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)')[0].text.squish
 						viewed = detail.css('.visitas-auto')[0].text.squish
 						element = {
