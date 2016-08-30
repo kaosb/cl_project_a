573318ce249a1667776ae02cb3f190eb1cf0f45d
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-08-30 15:09:55 -0300

    Fix cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ env :PATH, '/home/deploy/.gem/ruby/2.3.0'
 env :GEM_PATH, '/home/deploy/.rbenv/versions/2.3.1/lib/ruby/gems/2.3.0'
 
 every 10.minutes do
-	runner Car.alerta()
+	runner 'Car.alerta()'
 end
 
 every 1.day, :at => '2:30 am' do
